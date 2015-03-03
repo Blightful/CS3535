84e83823c66fa73691d71ff5a474c012f0c9f6d7
Walt <scarborowj@email.appstate.edu>
2015-03-03 13:12:03 -0500

    modules

---
+++ modules/Module 1/README.md
@@ -0,0 +1,25 @@
+timbreSim
+
+
+PROBLEM
+############
+I wanted to find the similarity between 2 songs based on their timbre
+values for an input audio file. The goal is to be able to find similar songs 
+given only their audio files or representation.
+
+PROCESS
+##############
+My code is a fairly simple script that takes two strings that represent audio files
+within the same directory as the '.py' file. It does this by using Gaussian Mixture Models and their scores to create a representation of any given segments probability to be found within the songs data. 
+
+Each length 12 array of timbre is simplified into score values (1 per segment) that represent the probability that a given point will be successfully fitted to the entire song. By getting this information for two different songs (in this example) and comparing the two against each other, you can find the relative timbre similarity for the two songs.
+
+Because the data was initially too small, the models had to be averaged rather than summed to create a value other than 0 (results were too far in the negatives to be brought back to beyond zero)
+
+DEPENDENCIES
+#############
+
+echonest.remix.audio
+pyechonest
+sklearn.mixture (available through scikit-learn)
+numpy
---
+++ modules/Module 1/timbreSim.py
@@ -0,0 +1,107 @@
+
+"""
+Author: Walt Scarboro
+Finds timbre similarity for 2 given mp3 based on the probability that data from
+one song will fall within the model for the other given song.
+
+#CURRENTLY NEEDS TUNING TO BE ABLE TO DISPLAY SIMILARITY IN MEANINGFUL MANNER#
+##############CURRENT VALUES MUST BE SORTED AGAINST OTHER VALUES##############
+
+Output values closer to 0 are more dissimilar.
+
+"""
+
+import math
+
+import pyechonest
+import echonest.remix.audio as audio
+import numpy
+from sklearn import mixture
+
+"""
+These are the two MP3's I used and the value that was outputted.
+Here I used Eleanor Rigby by The Beatles and Romance No.2 by Beethoven for the 
+similar sounding violin and sound. 
+
+For you own local audio files, simply change out the below MP3's for you own local files.
+"""
+# similarity = 0.002407
+audio_file = audio.LocalAudioFile('Eleanor Rigby.mp3')
+# audio_file = audio.LocalAudioFile('Romance2.mp3') 
+audio_file2 = audio.LocalAudioFile('Romance2.mp3')
+
+
+
+"""
+This is extracting the timbre into 2 separate 2D arrays and then 
+saving as a numpy array object of names a and b.
+"""
+timbre = audio_file.analysis.segments.timbre
+timbre2 = audio_file2.analysis.segments.timbre
+
+a = numpy.array(timbre)
+b = numpy.array(timbre2)
+
+lenA = len(a)
+lenB = len(b)
+print 'Song 1, number of segments: ' + str(lenA)
+print 'Song 2, number of segments: ' + str(lenB)
+
+
+
+"""
+Here I generate the models big A/B to compare to the timbre data sets a/b
+Comparisons between Aa, Ab, Ba, and Bb are generated.
+"""
+modelA = mixture.GMM(n_components=3, covariance_type='diag', random_state=None,
+ thresh=0.01, min_covar=0.001, n_iter=1000, n_init=1, params='wmc',
+  init_params='wmc')
+
+
+modelB = mixture.GMM(n_components=3, covariance_type='diag', random_state=None,
+ thresh=0.01, min_covar=0.001, n_iter=1000, n_init=1, params='wmc',
+  init_params='wmc')
+
+
+modelA.fit(a)
+modelB.fit(b)
+
+
+
+
+scoreAa = modelA.score(a)
+valAa = 0;
+for pt in range(0, len(scoreAa)):
+	valAa += scoreAa[pt]
+
+scoreAb = modelB.score(a)
+valAb = 0;
+for pt2 in range(0, len(scoreAb)):
+	valAb += scoreAb[pt2]
+
+
+scoreBa = modelA.score(b)
+valBa = 0;
+for pt3 in range(0, len(scoreBa)):
+	valBa += scoreBa[pt3]
+
+
+scoreBb = modelB.score(b)
+valBb = 0;
+for pt4 in range(0, len(scoreBb)):
+	valBb += scoreBb[pt4]
+
+
+print valAb
+print valAa
+print valBa
+print valBb
+
+prob = (valAb - valAa)/(2.00 * lenA) + (valBa - valBb)/(2.00 * lenB)
+
+print prob
+print math.exp(prob)
+
+
+
+		
